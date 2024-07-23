498bb9d8f385fb2bc249540872a6eed0cd15e51e
Murphy <1439290670@qq.com>
2024-07-24 01:22:58 +0800

    Add files via upload

--- Sensors.ino
+++ Sensors.ino
@@ -7,13 +7,17 @@
 #include <ArduinoJson.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <DHT.h>
 
 #define MQTT_VERSION MQTT_VERSION_3_1_1
+#define DHTTYPE DHT11   // 使用DHT 11温度湿度模块 
 #define MQ2_ANALOG_PIN A0  // ESP8266 的 ADC
 #define FIRE_DIGITAL_PIN 16 // ESP8266 的 GPIO16 (D0)
 #define BEEP D3
+#define DHTPIN D4      //定义DHT11模块连接管脚io5
 
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ D2, /* data=*/ D1, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
+DHT dht(DHTPIN, DHTTYPE);    //定义dht
 
 // Wifi: SSID and password
 const char* WIFI_SSID = "Xiaomi_705";
@@ -31,6 +35,7 @@ const char* MQTT_LIGHT_STATE_TOPIC = "kitchen/alarm/state";
 const char* MQTT_LIGHT_COMMAND_TOPIC = "kitchen/alarm/state";
 
 int wifi_sign = 0;
+float humi_read = 0, temp_read = 0;
 
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
@@ -81,11 +86,13 @@ int reconnect() {
   return 0;
 }
 
-void publishData(float p_MQ2) {
+void publishData(float p_MQ2,float p_temp,float p_humi) {
   // 使用最新版本的ArduinoJson库
   StaticJsonDocument<200> doc;
   // 将数据添加到JSON文档中
-  doc["MQ2"] = p_MQ2;
+  doc["GAS"] = p_MQ2;
+  doc["TEMP"] = p_temp;
+  doc["HUMI"] = p_humi;
 
   // 将JSON文档序列化为字符串
   char data[200];
@@ -115,24 +122,56 @@ float MQ2() {
   return analogValue;
 }
 
-void SSD1306(float analogValue, int sign1, int wifi_sign) {
-  char buffer[30];
+void SSD1306(float analogValue, int sign1, int wifi_sign,float h,float t) {
+  char buffer1[30];
+  char temp[30];
+  char humi[30];
   String formattedTime = timeClient.getFormattedTime();
-  sprintf(buffer, "天然气浓度：%.2f", analogValue);
+  sprintf(buffer1, "天然气浓度：%.2f", analogValue);
+  sprintf(temp, "温度：%.1f", t);
+  sprintf(humi, "湿度：%.1f", h);
   u8g2.setFont(u8g2_font_wqy16_t_gb2312);  // use a font that supports Chinese and numeric characters
   u8g2.setFontDirection(0);
   u8g2.clearBuffer();
-  u8g2.drawUTF8(0, 16, buffer);
-  if (sign1 == 1) u8g2.drawUTF8(8, 32, "MQTT连接失败");
-  if (wifi_sign == 3) u8g2.drawUTF8(32, 48, "配网失败");
+  u8g2.drawUTF8(0, 16, buffer1);
+  if (wifi_sign == 3) {u8g2.drawUTF8(0, 64, "NX");}
+  else if(sign1 == 1) {u8g2.drawUTF8(0, 64, "MX");}
+  u8g2.drawUTF8(24, 32, temp);
+  u8g2.drawUTF8(24, 48, humi);
   u8g2.drawStr(32, 64, formattedTime.c_str()); // drawStr can be used for standard ASCII characters
   u8g2.sendBuffer();
+}
+
+float temp(){
+  float t = dht.readTemperature();
+  if (isnan(t))
+  {
+    Serial.print("Failed to read Temperature!\n");
+  }
+  else
+  {
+    temp_read = t;
+  }
+  return t;
+}
+
+float humi(){
+  float h = dht.readHumidity();
+  if (isnan(h))
+  {
+    Serial.print("Failed to read Humidity!\n");
+  }
+  else
+  {
+    humi_read = h;
+  }
+  return h;
 }
 
 void setup() {
   // init the serial
   Serial.begin(115200);
-
+  dht.begin();
   // init the u8g2
   u8g2.begin();
   u8g2.enableUTF8Print();
@@ -178,6 +217,8 @@ void loop() {
 
   // 调用MQ2函数并获取返回值
   float mq2Value = MQ2() / 10.24;
+  float tempValue = temp();
+  float humiValue = humi();
 
   // 使用返回值调用publishData和SSD1306函数
   // SSD1306(mq2Value);
@@ -196,14 +237,71 @@ void loop() {
       digitalWrite(BEEP, beepState ? LOW : HIGH);
     }
     Serial.println("!!!WARNING!!!");
-    publishData(100);
+    publishData(100,tempValue,humiValue);
   } else {
     digitalWrite(BEEP, HIGH);
   }
 
   if (currentTime - lastUpdateTime >= 1000) { // 每秒更新一次屏幕和发送数据
     lastUpdateTime = currentTime;
-    SSD1306(mq2Value, sign1, wifi_sign);
-    publishData(mq2Value);
+    SSD1306(mq2Value, sign1, wifi_sign,humiValue,tempValue);
+    publishData(mq2Value,tempValue,humiValue);
   }
 }
+
+/*
+主题发布：
+Topic: homeassistant/sensor/kitchen_gas/config
+{
+  "name": "天然气浓度",
+  "device_class": "gas",
+  "state_topic": "kitchen/alarm/state",
+  "unit_of_measurement": "ppm",
+  "value_template": "{{ value_json.GAS }}",
+  "unique_id": "Kitchen_GAS",
+  "device": {
+    "identifiers": [
+      "ESP-12F"
+    ],
+    "name": "厨房传感器",
+    "model": "ESP-12F",
+    "manufacturer": "Powered By ZZH"
+  }
+}
+
+homeassistant/sensor/kitchen_humi/config
+{
+  "name": "湿度",
+  "device_class": "humidity",
+  "state_topic": "kitchen/alarm/state",
+  "unit_of_measurement": "%",
+  "value_template": "{{ value_json.HUMI }}",
+  "unique_id": "Kitchen_HUMI",
+  "device": {
+    "identifiers": [
+      "ESP-12F"
+    ],
+    "name": "厨房传感器",
+    "model": "ESP-12F",
+    "manufacturer": "Powered By ZZH"
+  }
+}
+
+homeassistant/sensor/kitchen_temp/config
+{
+  "name": "温度",
+  "device_class": "temperature",
+  "state_topic": "kitchen/alarm/state",
+  "unit_of_measurement": "℃",
+  "value_template": "{{ value_json.TEMP }}",
+  "unique_id": "Kitchen_TEMP",
+  "device": {
+    "identifiers": [
+      "ESP-12F"
+    ],
+    "name": "厨房传感器",
+    "model": "ESP-12F",
+    "manufacturer": "Powered By ZZH"
+  }
+}
+*/
